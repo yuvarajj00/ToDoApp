2437b382adbd13c623eaa170f7fbf472e03c71e5
yuvarajj00 <yuvarajuchitikela000@gmail.com>
2026-01-15 08:37:06 +0530

    change for PostGreSQL

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -13,7 +13,7 @@ namespace ToDoApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Register repositories
             builder.Services.AddScoped<ITaskRepository, TaskRepository>();
